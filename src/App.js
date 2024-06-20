34283f65c2a6cc73d8923e4ef0bd45fa8d231537
xmen <xdx70177@gmail.com>
2024-06-19 18:58:32 -0700

    c2

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import Header from "./components/Header";
+
+import axios from 'axios';
+
+const HeaderParams={
+     text:"MY FAVORITE GAMES"
+};
+const B=({text,onClick})=>{
+
+   return(
+    <>
+       <button onClick={onClick}>{text}</button>
+    </>
+   );
+};
+
+const click=function(){
+ 
+
+    const fetchData=async()=>{
+
+      try{
+        const res= await axios.get("http://kawkaw.wuaze.com/api/game/getGames");
+        alert(res.data);
+
+      }catch(error)
+      {
+        alert(error);
+      }
+
+    }
+
+    fetchData();
+
+};
+
+export default function App()
+{
+    return(
+      <>
+        <div className="container">
+        <Header  HeaderParams={HeaderParams}   />
+        <B text="click me" onClick={click} />
+        </div>
+      </>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+
+const button = document.createElement('button');
+
+
+button.textContent = 'Click me';
+
+
+button.addEventListener('click', () => {
+  alert('Button clicked!');
+});
+
+
+document.body.appendChild(button);*/
+
+
+
+root.render(
+<>
+
+<App /> 
+
+</>
+
+  /*
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>*/
+);
+
+/*
+reportWebVitals();*/
+
+
+
